4e723191bf5457539616f7d12ba8b09db9a48ca4
ohnix <65321469+ohnix@users.noreply.github.com>
2024-03-27 06:43:26 -0400

    made neovide look a lil better

--- init.lua
+++ init.lua
@@ -4,9 +4,11 @@ require("config.lazy")
 -- experimental neovide config
 vim.g.moonflyWinSeparator = 0
 vim.g.nightflyWinSeparator = 0
+
 if vim.g.neovide then
+  vim.o.guifont = "Fira Code:h12"
   -- scale factor
-  vim.g.neovide_scale_factor = 0.9
+  vim.g.neovide_scale_factor = 1
 
   -- transparency
   -- Helper function for transparency formatting
@@ -15,7 +17,7 @@ if vim.g.neovide then
   -- end
 
   -- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
-  -- vim.g.neovide_transparency = 0.8
+  vim.g.neovide_transparency = 0.8
   -- vim.g.neovide_transparency = 0.0
   -- vim.g.transparency = 0.9
   -- vim.g.neovide_background_color = "#000000" .. alpha()
